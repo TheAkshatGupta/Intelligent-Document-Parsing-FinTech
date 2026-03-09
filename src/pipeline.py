9f591fd470e00bc5948f1eaf1a21a5900facc711
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-09 13:45:17 +0530

    Updated pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,16 +3,18 @@
 
 from text_extraction import extract_text_from_txt
 from text_preprocessing import clean_text
+from entity_extraction import extract_entities
 
 
 def run_pipeline(file_path):
     raw_text = extract_text_from_txt(file_path)
     cleaned_text = clean_text(raw_text)
-    return cleaned_text
+    entities = extract_entities(cleaned_text)
+    return  entities
 
 
 if __name__ == "__main__":
     sample_file = "data/sample1.txt"
     output = run_pipeline(sample_file)
-    print("CLEANED TEXT OUTPUT:\n")
+    print("FINAL EXTRACTED ENTITIES:\n")
     print(output)
